54691b3e5aa2c50038c7724a3dff6574911cc28e
Hoang Cong Minh <hcminh@cmcglobal.vn>
2023-04-25 15:23:11 +0700

    Init homepage

--- lib/ui/home/home_viewmodel.dart
+++ lib/ui/home/home_viewmodel.dart
@@ -1,6 +1,23 @@
+import 'dart:async';
+
+import 'package:driver_license_test/data/model/movie.dart';
+import 'package:driver_license_test/data/repository/movie_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeViewModel extends ChangeNotifier {
+import '../../di/dependency_injection.dart';
 
+class HomeViewModel {
+  final _movieRepository = getIt.get<MovieRepository>();
+
+  StreamController movieStreamController = StreamController<Movie>.broadcast();
+  StreamController isShowLoading = StreamController<bool>.broadcast();
+
+  void getTrendingMovies() {
+    isShowLoading.sink.add(true);
+    _movieRepository.getTrendingMovie().then((movies) {
+      movieStreamController.sink.add(movies);
+      isShowLoading.sink.add(false);
+    });
+  }
 }
